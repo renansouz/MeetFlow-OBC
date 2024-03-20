2f95e74506e949882cf94dd0b8a29a133f694231
miqueiasmartinsf <miqueias.martinsf@gmail.com>
2024-03-20 10:44:14 -0300

    adjust footer toogle theme

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 import { useTheme } from "@/context/theme-provider";
-import Logo from "@/public/Logo.png";
-import LightLogo from "@/public/Logo-light.png";
+import OnlyLightLogo from "@/public/only-logo-white.png";
+import OnlyDarkLogo from "@/public/only-logo-black.png";
 import { Separator } from "@/components/ui/separator";
 
 import { Instagram, Github, Twitter, Youtube } from "lucide-react";
@@ -9,30 +9,32 @@ export const Footer = () => {
     const { theme } = useTheme();
 
     return (
-        <div className="flex justify-center flex-col items-center py-10 h-  pt-32">
+        <div className="flex justify-center flex-col items-center py-10 pt-32">
             <Separator
-                    orientation="horizontal"
-                    className="w-full h-0.5 mb-10"
-                />
+                orientation="horizontal"
+                className="w-full h-0.5 mb-24"
+            />
             <div className="w-5/6">
-                <div className="flex justify-center items-start w-full gap-32">
-                    <div className="flex flex-col bg-blue w-4/12 justify-center">
+                <div className="flex justify-evenly items-start w-full gap-32 flex-wrap">
+                    <div className="flex flex-col bg-blue w-2/12 justify-center items-start">
                         <img
-                            src={theme === "dark" ? Logo : LightLogo}
+                            src={
+                                theme === "dark" ? OnlyLightLogo : OnlyDarkLogo
+                            }
                             alt=""
-                            className="w-full"
+                            className="w-3/4"
                         />
-                        <p className="text-center">
+                        <p className="text-start">
                             Organize. Agende. Conecte-se. MeetFlow.
                         </p>
                     </div>
-                    <div className="flex flex-col gap-4 font-thin">
+                    <div className="flex flex-col gap-4">
                         <h2 className="text-2xl font-bold mb-2">Recursos</h2>
                         <a href="#">Termos de Uso</a>
                         <a href="#">Política de Privacidade</a>
                         <a href="#">Tutoriais</a>
                     </div>
-                    <div className="flex flex-col gap-4 font-thin">
+                    <div className="flex flex-col gap-4">
                         <h2 className="text-2xl font-bold mb-2">Sobre Nós</h2>
                         <a href="#">Quem Somos</a>
                         <a href="#">Nossa Missão</a>
@@ -40,7 +42,7 @@ export const Footer = () => {
                         <a href="#">Blog</a>
                         <a href="#">Newsletters</a>
                     </div>
-                    <div className="flex flex-col gap-4 font-thin">
+                    <div className="flex flex-col gap-4">
                         <h2 className="text-2xl font-bold mb-2">Suporte</h2>
                         <a href="#">FAQ</a>
                         <a href="#">Contate-nos</a>
