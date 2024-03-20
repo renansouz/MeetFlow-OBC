c8ddc881efb561c2d9df7ac9e14c36f6338cc45e
miqueiasmartinsf <miqueias.martinsf@gmail.com>
2024-03-20 16:08:59 -0300

    create register page

--- src/_layouts/auth.tsx
+++ src/_layouts/auth.tsx
@@ -1,14 +1,9 @@
 import { Outlet } from 'react-router-dom';
 
-import { Header } from '@/components/Header/header';
-
 export function AuthLayout() {
     return (
         <div className="flex min-h-screen flex-col antialiased">
-            <Header />
-            <p>teste</p>
-
-            <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
+            <div className="flex flex-1 flex-col gap-4">
                 <Outlet />
             </div>
         </div>
---
+++ src/pages/Register/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import pageBackground from '@/public/register-background.jpg';
+
+export const BackGroundDiv = styled.div`
+    background-image: linear-gradient(140deg, rgba(0, 0, 1, 0.8) 40%, rgba(26, 30, 95, 0.8)), url(${pageBackground});
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: 100%;
+    height: 100%;
+    background-position: center center;
+    display: flex;
+    gap: 50px;
+    z-index: 1;
+`;
+
+export const FormDiv = styled.div`
+    padding: 20px 0px;
+    background-color: white;
+    box-shadow: 30px 30px 0px #4338ca;
+    width: 60%;
+    height: 65%;
+`;
