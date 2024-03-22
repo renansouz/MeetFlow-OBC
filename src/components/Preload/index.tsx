d92ae341dda38c4a57cb1bdcbfe17a34ef88b3e1
miqueiasmartinsf <miqueias.martinsf@gmail.com>
2024-03-22 11:51:43 -0300

    update user profile page

--- src/components/Preload/index.tsx
+++ src/components/Preload/index.tsx
@@ -2,7 +2,7 @@ import logo from '../../public/faviconMeetFlow.png';
 
 const Preloader: React.FC = () => {
     return (
-        <div className="flex h-screen items-center justify-center bg-slate-950">
+        <div className="flex h-screen flex-col items-center justify-center bg-slate-950">
             <img src={logo} alt="Logo" className="h-20 w-20 animate-bounce" />
         </div>
     );
