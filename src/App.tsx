14884a3eaed97177a937b9b758b661a68426d2da
renansouz <renanss2005@gmail.com>
2024-03-22 01:30:48 -0300

    Add Preloader

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
+import React, { useEffect, useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
 
 import { ThemeProvider } from '@/context/theme-provider';
 import { router } from '@/router';
 
+import Preloader from './components/Preload';
 import { GlobalStyles } from './styles/global';
 
 function App() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+    }, []);
+
     return (
         <>
-            <ThemeProvider storageKey="MeetFlow-theme" defaultTheme="dark">
-                <GlobalStyles />
-                <RouterProvider router={router} />
-            </ThemeProvider>
+            {loading ? (
+                <Preloader />
+            ) : (
+                <ThemeProvider storageKey="MeetFlow-theme" defaultTheme="dark">
+                    <GlobalStyles />
+                    <RouterProvider router={router} />
+                </ThemeProvider>
+            )}
         </>
     );
 }
---
+++ src/components/Preload/index.tsx
@@ -0,0 +1,11 @@
+import logo from '../../public/faviconMeetFlow.png';
+
+const Preloader: React.FC = () => {
+    return (
+        <div className="flex h-screen items-center justify-center bg-slate-950">
+            <img src={logo} alt="Logo" className="h-20 w-20 animate-bounce" />
+        </div>
+    );
+};
+
+export default Preloader;
