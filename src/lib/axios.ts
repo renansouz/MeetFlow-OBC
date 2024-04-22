6c87b9c481c584d2ae3e68e422abd723c6427f1b
WesleyR10 <wesleyribas2015@gmail.com>
2024-04-22 15:33:48 -0300

    Testing lib cookies

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,10 +1,13 @@
 import axios from 'axios';
-import Cookies from 'js-cookie';
+import { parse } from 'cookie';
 
 import { env } from '@/env';
 
 export function setupAPIClient() {
-  const token = Cookies.get('meetFlow.token');
+  const cookies = parse(document.cookie);
+  console.log('cookies setupAPIClient', cookies);
+  const token = cookies['meetFlow.token'];
+  console.log('token setupAPIClient', token);
   const api = axios.create({
     baseURL: env.VITE_BASE_URL,
     headers: { authorization: `Bearer ${token}` },
@@ -14,3 +17,17 @@ export function setupAPIClient() {
 }
 
 export const api = setupAPIClient();
+
+export function updateToken() {
+  const cookies = parse(document.cookie);
+  const token = cookies['meetFlow.token'];
+  if (token) {
+    console.log('token updateToken if', token);
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    console.log('token updateToken else', token);
+    delete api.defaults.headers.common['Authorization'];
+  }
+}
+
+updateToken();
